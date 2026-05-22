b7954d7ffd0ad58def7c3875969010524a7d0259
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-05-22 08:07:26 +0500

    coding

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ const App = () => {
   return (
     <div>
       <h1>rafay</h1>
-      
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quo debitis eligendi iste delectus fuga, velit perferendis ipsam temporibus mollitia aliquid nesciunt aut commodi vitae, possimus est? Voluptatem, est facilis?</p>
+      all the best for you
     </div>
   )
 }
